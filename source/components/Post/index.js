dfdd37133e3a44ce15b873b0c32371438ed364c3
Denis Ostrovsky <4deni.kiev@gmail.com>
2018-09-28 15:57:09 +0300

    lesson 4 homework

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -2,24 +2,25 @@
 import React, { Component } from 'react';
 import moments from 'moment';
 
+// Components
+import { Consumer } from 'components/HOC/withProfile'
+
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Post extends Component {
     render() {
-        const {
-            currentUserFirstName,
-            currentUserLastName,
-            avatar
-        } = this.props;
-
-        return (        
-            <section className = { Styles.post }>
-                <img src = { avatar } />
-                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
-                <time>{ moments().format('MMMM D h:ms:ss a') }</time>
-                <p>Howdy!</p>
-            </section>
+        return (
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.post }>
+                        <img src = { context.avatar } />
+                        <a>{ context.currentUserFirstName } { context.currentUserLastName }</a>
+                        <time>{ moments().format('MMMM D h:ms:ss a') }</time>
+                        <p>Howdy!</p>
+                    </section>
+                )}
+            </Consumer>
         )
     }
 }
